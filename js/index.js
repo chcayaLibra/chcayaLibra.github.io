0e0f3fb02e6d65f897ca09d277d864eed0b1b5eb
chcaya <chen.3514612746@gmail.com>
2025-01-25 23:22:29 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//资源加载
 window.addEventListener('load', function () {
   const loading = document.querySelector('.loading');
   setTimeout(function () {
@@ -6,6 +7,7 @@ window.addEventListener('load', function () {
   clearInterval(loadingReset);
 });
 
+// 加载动画
 const loading = document.querySelectorAll('.loading .animation');
 let loadingReset = setInterval(function () {
   for (let i = 0; i < loading.length; i++) {
@@ -17,10 +19,25 @@ let loadingReset = setInterval(function () {
   console.log(loadingReset);
 }, 6000);
 
+// 阻止冒泡
+const shade = document.querySelector('.shade');
+shade.addEventListener('click', function (e) {
+  e.stopPropagation();
+})
 
+// 角色移动 / 点击特效
 const n = document.documentElement;
+const click = document.querySelector('.click');
 
 const role = document.querySelector('.role');
 n.addEventListener('click', function (e) {
-  role.style.left = `${e.screenX - role.clientWidth / 2}px`;
+  role.style.left = `${e.offsetX - role.clientWidth / 2}px`;
+
+  click.style.left = `${e.offsetX - click.clientWidth / 2}px`;
+  click.style.top = `${e.offsetY - click.clientHeight / 2}px`;
+
+  click.classList.add('click-animation');
+  setTimeout(function () {
+    click.classList.remove('click-animation');
+  }, 500);
 });
