a1a232c92f19d6de4fd7d45209a8284f160ece79
chcaya <chen.3514612746@gmail.com>
2025-01-25 22:06:58 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const loading = document.querySelector('.loading');
+  setTimeout(function () {
+    loading.style.opacity = 0;
+  }, 2000);
+  clearInterval(loadingReset);
+});
+
+const loading = document.querySelectorAll('.loading .animation');
+let loadingReset = setInterval(function () {
+  for (let i = 0; i < loading.length; i++) {
+    loading[i].classList.remove('animation');
+    setTimeout(function () {
+      loading[i].classList.add('animation');
+    }, 10)
+  }
+  console.log(loadingReset);
+}, 6000);
+
+
 const n = document.documentElement;
 
 const role = document.querySelector('.role');
